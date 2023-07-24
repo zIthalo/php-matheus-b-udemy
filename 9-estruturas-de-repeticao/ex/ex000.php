29967acfc24bcbbddd3fc82aa4d5701b4f3f77de
zIthalo <ithalowillian@gmail.com>
2023-07-23 22:54:17 -0300

    Update ex000.php

--- 9-estruturas-de-repeticao/ex/ex000.php
+++ 9-estruturas-de-repeticao/ex/ex000.php
@@ -3,7 +3,8 @@ $arr = ['carro', 13, 'moto', false, 'barco', 'avião', true, 'foguete', 23];
 
 $x = count($arr);
 $y = 0;
-while ($y <= $x) {
+echo '<h3>Imprimindo somente as STRINGS do array.</h3>';
+while ($y < $x) {
     if (is_string($arr[$y])) {
         echo "$arr[$y] <br>";
     }
