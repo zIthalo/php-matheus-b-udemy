bd5b2c29683966e43fddec55f85776513b9cbf9d
zIthalo <ithalowillian@gmail.com>
2023-08-14 12:02:44 -0300

    Introdução ao Banco de Dados + PHP

---
+++ 18-php-bd/1-php-banco-de-dados/index.php
@@ -0,0 +1,23 @@
+<?php 
+/*
+    - O PHP tem suporte para mais de 20 bancos de dados;
+    
+    - O mais comum a ser utilizado é o MySQL;
+
+    - Há algumas formas de conexão a bancos disponíveis no PHP, a mias famosa é o PDO (PHP Data Objects), porém também temos a mysqli;
+
+    - PDO costuma ser implementado por causa da abordagem orientada a objetos e outras vantagens sobre a mysqli;
+
+        ***BANCO DE DADOS RELACIONAIS***
+    - O banco de dado relacional tem sua principal característica trabalhar com tabelas;
+ 
+    - onde ela possui colunas que categorizam os dados, que são inseridos nas tabelas
+ 
+    - O php é muito utilizado com DBs relacionais, como o MySQL;
+ 
+    - A linguagem para operações com estes bancos é a SQL;
+ 
+    - As instruções SQL costumam ser escritas em letras maiúsculas;
+*/
+
+?>
---
+++ 18-php-bd/2-criando-banco-de-dados/index.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+    - Podemos criar bancos de dados manualmente em softwares como o phpMyAdmin ou por SQL;
+ 
+    - Neste curso, com o intuito de aprender SQL, vamos utilizar os comandos de query que vão servir ao longo da sua carreira de programação;
+ 
+    - Para criar bancos vamos utilizar o:
+        CREATE DATABASE nomedobanco;
+ 
+
+*/
+?>
